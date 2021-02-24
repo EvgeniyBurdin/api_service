dfd7d877b706847e33fd156e06553cf3dd3bde25
Evgeniy Byrdin <e.s.burdin@yandex.ru>
2021-02-24 20:21:04 +0400

    Сделал формирование ошибки сразу в словарь

--- sources/middlewares/wrap_json.py
+++ sources/middlewares/wrap_json.py
@@ -4,7 +4,6 @@
 from typing import Any, Callable
 
 from aiohttp import web
-from data_classes.errors import ErrorResponse
 from data_classes.wraps import WrapRequest, WrapResponse
 from valdec.errors import ValidationArgumentsError
 
@@ -24,19 +23,19 @@ class WrapExample(KwargsHandler):
     """
 
     def get_error_body(self, request: web.Request, error: Exception) -> dict:
-
-        result = ErrorResponse(
-            error_type=str(type(error)), error_message=str(error)
-        )
-        response = WrapResponse(
+        """ Формирует и отдает сообщение об ошибке.
+        """
+        # Так как мы знаем какая у нас оболочка ответа, сразу сделаем словарь
+        # с аналогичной "схемой"
+        result = dict(error_type=str(type(error)), error_message=str(error))
+        response = dict(
             success=False,
             result=result,
             # Достанем id, если он был сохранен в методе run_handler()
             id=request.get(KEY_NAME_FOR_ID)
         )
-
         # Отдаем всегда словарь
-        return response.dict()
+        return response
 
     async def run_handler(
         self, request: web.Request, handler: Callable, request_body: Any
